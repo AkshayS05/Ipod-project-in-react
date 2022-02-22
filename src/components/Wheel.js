bd82dd034e1d6a99bbfd2da785c5beb00b01979b
akshay <akshaysharma5432@gmail.com>
2022-02-22 22:27:34 +0530

    style wheel part

---
+++ src/components/Wheel.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Wheel.css";
+
+//icons
+import leftIcon from "./icons";
+export default function Wheel() {
+  return (
+    <div>
+      <div className="outerSurface">
+        <div className="buttonControls"></div>
+      </div>
+    </div>
+  );
+}
