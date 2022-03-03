b791678dfaaee3eb48b788cd33ef5de26642a23b
akshay <akshaysharma5432@gmail.com>
2022-03-03 23:07:10 +0530

    remove image backicon from all files

--- src/components/Music.js
+++ src/components/Music.js
@@ -3,15 +3,11 @@ import React from "react";
 //css file
 import "./Music.css";
 // icon
-import musicIcon from "../assets/musicIcon.svg";
 
 export default function Music() {
   return (
     <div className="music">
-      <img src={musicIcon} />
-      <div className="back">
-        <img src={backIcon} />
-      </div>
+      <div className="back"></div>
     </div>
   );
 }
